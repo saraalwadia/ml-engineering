e9db3041ae06b09a778608569fac7585bcd0fb43
Sara Alwadia <sara.wadia2016@gmail.com>
2026-01-21 13:15:46 +0200

    add new root

--- 01-Supervised-Learining-with-sklearn/04-preprocessing-data/scripts/centering-with-pipeline.py
+++ 01-Supervised-Learining-with-sklearn/04-preprocessing-data/scripts/centering-with-pipeline.py
@@ -1,3 +1,17 @@
+# Import StandardScaler
+from sklearn.preprocessing import StandardScaler
+
+# Create pipeline steps
+steps = [("scaler", StandardScaler()),
+         ("lasso", Lasso(alpha=0.5))]
+
+# Instantiate the pipeline
+pipeline =  Pipeline(steps)
+pipeline.fit(X_train, y_train)
+
+# Calculate and print R-squared
+print(pipeline.score(X_test, y_test))
+
 # Build the steps
 steps = [
     ("scaler", StandardScaler()),
