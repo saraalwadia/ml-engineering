690b74177e97e49f1f667de473ff7bc7f3e6284d
Sara Alwadia <sara.wadia2016@gmail.com>
2026-03-12 14:14:10 +0200

    add new python files

--- 06-Introduction-to-MLflow/10.py
+++ 06-Introduction-to-MLflow/10.py
@@ -3,3 +3,11 @@ mlflow.register_model("model_2022", "Insurance")
 
 # Register the second (2023) model
 mlflow.register_model(f"runs:/{run_id}/model_2023", "Insurance")
+
+# Log the model using scikit-learn flavor
+mlflow.sklearn.log_model(lr, "model", registered_model_name="Insurance")
+
+insurance_filter_string = "name = 'Insurance'"
+
+# Search for Insurance models
+print(client.search_registered_models(filter_string=insurance_filter_string))
